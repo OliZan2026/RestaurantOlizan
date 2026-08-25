b810737b24664680ff7a5c40dbba88566e91eaf1
OliZan2026 <sanierungstein@gmail.com>
2026-08-26 00:02:04 +0200

    Optimizează viteza paginii principale pe mobil

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 
 "use strict";
 
-const VERSIUNE = "1787684146972";
+const VERSIUNE = "1787695294133";
 
 const CACHE_SCOICA = "olizan-scoica-" + VERSIUNE;   /* interfața de bază */
 const CACHE_STATIC = "olizan-static-" + VERSIUNE;   /* stiluri, scripturi, fonturi, imagini */
