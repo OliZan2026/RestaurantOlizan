6c7c32695904ab4c10eea270c39113b34d008e23
OliZan2026 <sanierungstein@gmail.com>
2026-08-26 08:30:31 +0200

    Înlocuiește fotografia clădirii cu sala OLIZAN

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 
 "use strict";
 
-const VERSIUNE = "1787695940549";
+const VERSIUNE = "1787725799292";
 
 const CACHE_SCOICA = "olizan-scoica-" + VERSIUNE;   /* interfața de bază */
 const CACHE_STATIC = "olizan-static-" + VERSIUNE;   /* stiluri, scripturi, fonturi, imagini */
